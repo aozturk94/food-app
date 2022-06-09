f5090731eb5c9e378463dd898c3e9e95a8b184a6
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-06-09 14:55:05 +0300

    update redux

--- server/index.js
+++ server/index.js
@@ -3,19 +3,23 @@ const app = express();
 const db = require("./db");
 const cors = require("cors");
 const FoodModel = require("./models/FoodModel");
+const foodsRoute = require("./routes/foodsRoute")
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/getfoods", (req, res) => {
-  FoodModel.find({}, (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+// app.get("/getfoods", (req, res) => {
+//   FoodModel.find({}, (err, result) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
+
+app.use('/api/foods', foodsRoute);
 
 app.listen(1001, () => {
   console.log("server bir şekilde çalışıyor :))))");
